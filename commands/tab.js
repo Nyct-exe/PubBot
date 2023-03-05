42c300de1d2284d439186c416b16477900e144fa
Lukas <lukas.krasauskis123@gmail.com>
2023-03-05 22:38:50 +0000

    Updated tab.js to use defined function of user instead

--- commands/tab.js
+++ commands/tab.js
@@ -1,56 +1,33 @@
 const { SlashCommandBuilder } = require('discord.js');
-const { Drink, User } = require('../dbObjects.js');
+const { User } = require('../dbObjects.js');
 const { EmbedBuilder } = require('discord.js');
-const Sequelize = require('sequelize');
-
-const sequelize = new Sequelize('database', 'user', 'password', {
-	host: 'localhost',
-	dialect: 'sqlite',
-	logging: false,
-	// SQLite only
-	storage: 'database.sqlite',
-});
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('debt')
+		.setName('tab')
 		.setDescription('Gives a list of people you owe drinks'),
 	async execute(interaction) {
 		// Finds all people the message author owes drinks
-		const drinks = await Drink.findAll({
-			attributes: [
-				[sequelize.col('recipient.username'), 'recipient_username'],
-				[sequelize.col('buyer.username'), 'buyer_username'],
-				[sequelize.fn('COUNT', sequelize.col('*')), 'drink_count'],
-			],
-			include: [
-				{ model: User, as: 'recipient', attributes: [] },
-				{ model: User, as: 'buyer', attributes: [] },
-			],
-			where: {
-				'$recipient.userId$': interaction.member.id,
-			},
-			group: ['buyer.userId'],
-			having: sequelize.where(sequelize.fn('COUNT', sequelize.col('*')), '>', 0),
-			raw: true,
-		});
-
-		const messageEmbed = new EmbedBuilder()
-			.setColor(0x0099FF)
-			.setTitle('Beverage Debts')
-			.setDescription('List of people and how many drinks they bought you');
-		if (drinks.length === 0) {
-			messageEmbed.addFields(
-				{ name: 'Message', value: 'Your Tab Is Empty', inline: true },
-			);
-		}
-		else {
-			drinks.forEach((drink) => {
+		const user = await User.findOne({ where: { userId: interaction.member.id } });
+		if (user) {
+			const userTab = await user.getTab();
+			const messageEmbed = new EmbedBuilder()
+				.setColor(0x0099FF)
+				.setTitle('Pub Tab')
+				.setDescription('List of people and how many drinks they bought you');
+			if (userTab.length === 0) {
 				messageEmbed.addFields(
-					{ name: `User: ${drink.buyer_username}`, value: `Drinks Bought: ${drink.drink_count}` },
+					{ name: 'Message', value: 'Your Tab Is Empty', inline: true },
 				);
-			});
+			}
+			else {
+				userTab.forEach((drink) => {
+					messageEmbed.addFields(
+						{ name: `User: ${drink.buyer_username}`, value: `Drinks Bought: ${drink.drink_count}` },
+					);
+				});
+			}
+			await interaction.reply({ embeds: [messageEmbed], ephemeral: true });
 		}
-		await interaction.reply({ embeds: [messageEmbed], ephemeral: true });
 	},
 };
